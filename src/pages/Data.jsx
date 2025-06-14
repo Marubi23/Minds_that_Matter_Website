0f0f7b2bf87f800d32f1e2fa1c8f4871ce201c86
Marubi 254 <felixmarubi2005@gmail.com>
2025-06-14 22:59:52 +0300

    turns

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -63,7 +63,7 @@ const handleStudentSubmit=(e)=>{
         </div>
       <div className="logo-company"> <img style={{height:'200px',width:'180px'}}  src={logo} alt="company-logo" />
       <div className="portal">
-    <Link to={'/parent'}><button style={{padding:'20px',background:'hsl(151, 93.60%, 51.00%)',color:'black',border:'none',borderRadius:'10px',cursor:'pointer',fontFamily:'verdana'}}>Parent Access </button></Link>
+    <Link to={'/parent'}><button style={{padding:'20px',background:'hsl(151, 93.60%, 51.00%)',color:'black',border:'none',borderRadius:'10px',cursor:'pointer',fontFamily:'verdana'}}>Parental Access </button></Link>
       <Link to={'/teacher-login'}><button style={{padding:'20px',background:'hsl(272, 74.10%, 39.40%)',color:'white',border:'none',borderRadius:'10px',cursor:'pointer',fontFamily:'verdana'}}>Psychiatrist Access</button></Link>
       </div>
       </div>
